c5113ff9454f86b514a331adb9f50817e90d2ed9
pswar <spottavathini@ebay.com>
2013-03-06 14:13:56 -0800

    First commit!

---
+++ coding/forloop.js
@@ -0,0 +1,8 @@
+/*
+ * Main purpose is to build library of examples for most optimized coding styles with the help of community.
+ * This file contains examples for For-loop
+ */
+
+for (var i=0,len=list.length; i<len; i++) {
+	
+}
