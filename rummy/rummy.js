8636c4e6d3c442e2220c57d0e11a7588cc4bb254
Sathish Pottavathini <sathish@pottavathini.com>
2015-04-12 22:22:12 -0700

    Add more features to Rummy

--- rummy/rummy.js
+++ rummy/rummy.js
@@ -37,7 +37,8 @@ function startNewGame() {
 	for (var i=0; i<count; i++) { 
 		score[i] = {
 				'name' : 'Player' + i,
-				'rounds' : []
+				'rounds' : [],
+				're' : []
 			}
 	}
 	amplify.store( key, {'key': key, 'count': count, 'score': score, 'startedAt':new Date().getTime()} );
@@ -45,6 +46,10 @@ function startNewGame() {
 	paintScore(key);
 }
 
+function resetGame() {
+	amplify.store(CURRENT_KEY, null);
+	location.reload();
+}
 function paintScore(key) {
 	var currentGame = amplify.store(key);
 	if (!currentGame) return;
@@ -56,36 +61,36 @@ function paintScore(key) {
 
 	if (currentGame.score[0]) {
 		h += '<tr>';
-		h += '<th>Round</th>'
+		h += '<th> </th>'
 		for (var i=0; i < count; i++) {
 			var name = "p" + i;
 			var val = (currentGame.score[i])?currentGame.score[i].name:'Player'+i;
 			h += '<th><input type="text" name="' + name + '" id="' + name + '" value="' + val + '" onblur="updatePlayer(' + i + ')" /></th>';
 		}
-		h += '<td>';
-		h += '</td>';
+		h += '<th>';
+		h += '</th>';
 		h += '</tr>';
 
 		for (var i=0, len=currentGame.score[0].rounds.length; i<len; i++) {
 			if (currentGame.score[0].rounds[i] == null) continue;
 
 			h += '<tr>';
-			h += '<td></td>';
+			h += '<td>#' + (i+1) + '</td>';
 			for (var j=0, jLen=currentGame.score.length; j < jLen; j++) {
-				h += '<td>';
 				if (currentGame.score[j].rounds[i] == 0) {
-					h += '&reg;';
+					h += '<td style="color:green;font-weight:bold;">&reg;</td>';
+				} else if (currentGame.score[j].rounds[i] < 0) {
+					h += '<td style="color:#85083A">Re-entry</td>';
 				} else {
-					h += currentGame.score[j].rounds[i];
+					h += '<td>' + currentGame.score[j].rounds[i] + '</td>';
 				}
-				h += '</td>';
 			}
-			h += '<td><a href="#RemoveThisRow" onclick="removeRow(' + i + ');return false;">Remove</a></td>';
+			h += '<td style="font-size:small"><a href="#RemoveThisRow" onclick="removeRow(' + i + ');return false;">Remove</a></td>';
 			h += '</tr>';
 		}
 
 		//Totals
-		h += '<tr style="background-color:#81F781">';
+		h += '<tr class=total>';
 		h += '<td>';
 		h += 'Total: ';
 		h += '</td>';
@@ -100,6 +105,8 @@ function paintScore(key) {
 			var mx = parseInt($('#tot').val());
 			if (!isNaN(mx) && total > mx) {
 				h += '<strike style="color:red">' + total + '</strike>';
+				var reN = 're'+i;
+				h += '<br><a href="#Re-entry" onclick="reEntry(\''+i+'\');return false;">Re-entry</a> for <input width="20px" type=number id="'+reN+'" name="'+reN+'" value="' + getHiScore(currentGame) + '">';
 			} else {
 				var pack = parseInt($('#pack').val());
 				if (!isNaN(pack) && !isNaN(mx) && mx-total < pack) {
@@ -121,7 +128,10 @@ function paintScore(key) {
 	h += '</td>';
 	for (var i=0; i < count; i++) {
 		var name = "s" + i;
-		h += '<td><input type="text" name="' + name + '" id="' + name + '" value="" /></td>';
+		h += '<td>';
+		h += '<input type="number" name="' + name + '" id="' + name + '" value="" />';
+		h += '<a href="#pack" onclick="$(\'#' + name + '\').val($(\'#pack\').val());return false;">Pack</a>';
+		h += '</td>';
 	}
 	h += '<td>';
 	h += '<input type="button" name=add value="Add score" onclick="addScore()">';
@@ -132,6 +142,79 @@ function paintScore(key) {
 	$('#score').html(h);
 }
 
+function reEntry(index) {
+	var elem = 're'+index;
+	var score = parseInt($('#'+elem).val());
+	if (isNaN(score)) {
+		alert('Invalid entry!');
+		$('#'+elem).focus();
+		return;
+	}
+	//alert(score);
+	var key = CURRENT_KEY;
+	var currentGame = amplify.store(key);
+	if (!currentGame) {
+		alert('Something went really bad! Unable to retrieve the data. :(');
+		return;
+	}
+	var count = currentGame.count;
+
+	for (var i=0; i<count; i++) {
+
+		if (!currentGame.score[i]) {
+			currentGame.score[i] = {
+				'name' : $('#p' + i).val(),
+				'rounds' : []
+			}
+		}
+		var total = 0;
+		$.each(currentGame.score[i].rounds,function(a, b) {
+			if (b == null) return;
+		    total += b;
+		});
+
+		var val = 0;
+		if (i == index) {
+			val = score-total;
+		}
+		currentGame.score[i].rounds[currentGame.score[i].rounds.length] = val;
+	}
+
+	amplify.store( key, 
+		{	
+			'key': currentGame.key, 
+			'count': currentGame.count, 
+			'score': currentGame.score, 
+			'startedAt':currentGame.startedAt
+		} 
+	);
+
+	paintScore(key);
+
+}
+
+function getHiScore(currentGame) {
+	var count = currentGame.count;
+	var hi = 2;
+	for (var i=0; i<count; i++) {
+		var total = 0;
+		$.each(currentGame.score[i].rounds,function(a, b) {
+			if (b == null) return;
+		    total += b;
+		});
+		var mx = parseInt($('#tot').val());
+		if (!isNaN(mx) && total <= mx && total > hi) {
+			hi = total;
+		}
+
+	}
+
+	return hi;
+}
+
+function addPack(elem) {
+	$('#' + elem).val($('#pack').val());
+}
 function addScore() {
 	var key = CURRENT_KEY;
 	var currentGame = amplify.store(key);
